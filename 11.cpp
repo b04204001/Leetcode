5eca9ffa3189bf7f7568ade448a98c8ae06d2d10
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-04-05 16:56:18 +0800

    Create 11.cpp
    20220405 daily

---
+++ 11.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        if(height.size()==0){
+            return 0;}
+        else{
+        int left = 0;
+        int right = height.size() - 1;
+        int maxa = 0;
+        while(left < right){
+            int w = (right - left);
+            int h = min(height[left], height[right]);
+            int area = w * h;
+            maxa = max(maxa, area);
+            if(height[left] < height[right]) 
+                left++;
+            else if(height[left] > height[right]) 
+                right--;
+            else{
+                left++;
+                right--;
+            }
+        }
+        return maxa;
+        }
+    }
+};

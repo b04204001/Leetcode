67ce9ec00b6047f27dfcea1e63111cafe92ef9c8
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-04-11 23:52:17 +0800

    20220411

---
+++ 1260.cpp
@@ -0,0 +1,33 @@
+//20220411 daily 
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> ans(m,vector<int>(n));
+        if(k ==0)
+            return grid;
+        while(k>0){
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(i != m-1 && j == n-1){
+                        ans[i+1][0]=grid[i][j];
+                    }
+                    else if(i == m-1 && j == n-1){
+                        ans[0][0]=grid[i][j];
+                    }
+                    else{
+                        ans[i][j+1]=grid[i][j];
+                    }
+                }
+            }
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    grid[i][j]=ans[i][j];
+                }
+            }
+            k--;
+        }
+        return ans;
+    }
+};

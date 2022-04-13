9eb0303bff4efb6f65a51e3a7df2ff6ec078e68d
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-04-13 13:07:27 +0800

    wrong

--- 59.cpp
+++ 59.cpp
@@ -31,6 +31,35 @@ public:
             }
             if( flag[i][j] == 1){
                 turn = (turn +1)%4;  
+                int back =turn -1;
+                switch(back){
+                case 0:
+                    i++;
+                    break;
+                case 1:
+                    j--;
+                    break;
+                case 2:
+                    i--;
+                    break;
+                case 3:
+                    j++;
+                    break;
+                }
+                switch(turn){
+                case 0:
+                    j++;
+                    break;
+                case 1:
+                    i++;
+                    break;
+                case 2:
+                    j--;
+                    break;
+                case 3:
+                    i--;
+                    break;
+                }
             } 
             k++;
         }
@@ -39,14 +68,4 @@ public:
 };
 // || (i % n == (n-1)) || ( j % n == (n-1) ) 
 
-/* 1   2  3  4
-   12 13 14  5
-   11 16 15  6
-   10  9  8  7
-   
-   1   2  3  4  5
-   16 17 18 19  6
-   15 24 25 20  7
-   14 23 22 21  8
-   13 12 11 10  9*/
 

4a2a2ef063af0aa1c8eed78da233ec1c8852af62
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-04-14 13:18:03 +0800

    20220414

---
+++ 700.cpp
@@ -0,0 +1,27 @@
+//20220414 daily
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode *current = root;
+        while (current != NULL && val != current->val) {                                        
+                if (val < current->val){                      
+                    current = current->left;   
+                }
+                else {
+                    current = current->right;  
+                }
+            }
+            return current;
+        }
+};

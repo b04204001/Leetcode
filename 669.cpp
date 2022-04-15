98da3181441684daee268a754e9216596c300f29
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-04-15 19:37:11 +0800

    20200415 解答版 不熟tree

--- 669.cpp
+++ 669.cpp
@@ -12,16 +12,17 @@
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        if(!root) return nullptr;
-        if(root->val < low) {
-            return trimBST(root->right, low, high);
-        } else if (root->val > high) {
-            return trimBST(root->left, low, high);
+      if(!root)return NULL;
+        if(root->val<low){
+            root->left=NULL;
+            return trimBST(root->right,low,high);
         }
-        
-        root->left = trimBST(root->left, low, high);
-        root->right = trimBST(root->right, low, high);
-        
+        else if(root->val>high){
+            root->right=NULL;
+            return trimBST(root->left,low,high);
+        }
+        root->left=trimBST(root->left,low,high);
+        root->right=trimBST(root->right,low,high);
         return root;
     }
 };

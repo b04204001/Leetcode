ce0b157e1e67f1e9c353b480dc6e1d8d0a2838fd
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-04-20 23:59:29 +0800

    20220420

--- 230.cpp
+++ 230.cpp
@@ -1,1 +1,30 @@
-20200418
+//20200418
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans;
+    int kthSmallest(TreeNode* root, int &k) {
+        if(root){
+            kthSmallest(root->left,k);
+            if(--k == 0){
+                ans=root->val;
+                return ans;
+            }
+            kthSmallest(root->right,k);
+        }
+        return ans;
+    }
+};
+
+

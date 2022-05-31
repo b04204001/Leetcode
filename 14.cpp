b5c66e458f934509c1650d82ca30a0f34719b66a
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-05-31 19:03:36 +0800

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        int i=0;
+        int n = strs.size();
+        sort(strs.begin(),strs.end());
+        for(int i=0;i<min(strs[0].size(), strs[n-1].size() ) ;i++){
+            if(strs[0][i] == strs[n-1][i]){
+                ans += strs[0][i];
+            }
+            else{
+                break;
+            }   
+        }
+        return ans;
+    }
+};

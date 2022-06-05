927ce33f35a36d88d09351f10cb933fe82c681a4
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-06-05 21:55:44 +0800

    Create 35.cpp

---
+++ 35.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int ans;
+        for(int i = 0;i<nums.size();i++){
+            if(target <= nums[i]){
+                ans =i;
+                break;
+            }
+            
+        }
+        return ans;
+    }
+};

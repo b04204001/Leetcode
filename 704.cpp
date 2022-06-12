c16da4bce6f9a2a202147c1e8d1a08b62a8c90d3
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-06-13 01:59:35 +0800

    Update 704.cpp

--- 704.cpp
+++ 704.cpp
@@ -1,9 +1,8 @@
-TLE
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0, high = nums.size()-1;
-        int ans=-1;
+        int low=0, high = nums.size()-1,ans=-1;
         while(low <=high){
             int mid = (high+low)/2;
             if( target == nums[mid]){
@@ -11,10 +10,10 @@ public:
                 break;
             }
             else if( target > nums[mid]){
-                low = mid;
+                low = mid+1;
             }
             else{
-                high = mid;
+                high = mid-1;
             }
         }
 

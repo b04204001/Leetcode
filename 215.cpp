f6c96a5fa2a6ca8d1770a17b101bd211462ce0bb
b04204001 <60873981+b04204001@users.noreply.github.com>
2022-06-22 14:22:49 +0800

    Create 215.cpp

---
+++ 215.cpp
@@ -0,0 +1,9 @@
+20220622
+  class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int ans = nums[ nums.size() - k];
+        return ans;
+    }
+};

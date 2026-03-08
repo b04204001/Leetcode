793800180145a140c1646b4850a5823b25e12e51
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-08 22:25:21 +0800

    Create 190.cpp

---
+++ 190.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans =0;
+        for (int i =0 ; i<32;i++){
+            int bit = n & 1;
+            ans <<= 1;
+            ans |= bit;
+            n >>= 1;
+        }
+        return ans;
+    }
+};

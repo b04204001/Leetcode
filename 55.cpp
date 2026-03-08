56eb2a52418906c84488c00a171f8c2688509014
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-09 01:50:47 +0800

    Create 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int g = nums.size() - 1;
+        for (int i =g-1;i>=0;i--){
+            if(nums[i] + i >= g ){
+                g = i;
+            }
+        }
+        return g==0;
+    }
+};

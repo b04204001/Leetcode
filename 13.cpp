38f5e7b02a74f993bb6f21d40b5f54d39d971f01
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-09 20:10:19 +0800

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        // 1. 建立 ASCII 點名表當作 Hash Map (查詢時間 O(1))
+        int roman[128] = {0};
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+        int sum = 0;
+        for (int i=0;i< s.length();i++){
+            //正常+
+            if(roman[s[i]] < roman[s[i+1]] && i < (s.length() -1 )){
+                sum -= roman[s[i]];
+            }
+            //不正常-
+            else{
+                sum += roman[s[i]];
+            }
+        }
+
+        return sum;
+    }
+};
+
+
+
+2.
+
+    
+class Solution {
+public:
+    int romanToInt(string s) {
         int ans=0;
         for(int i=s.length()-1;i>=0;i--){
             if(s[i] == 'I'){

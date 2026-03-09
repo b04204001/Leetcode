2af9f882598dfecc485f8193d9765c23c346b69e
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-09 19:29:44 +0800

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string alph[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int dol[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}; 
+        int ans[13];
+        string a = "";
+        for (int i=0; i<13;i++){
+            ans[i] = num / dol[i];
+            num %= dol[i];
+        }
+        for (int i=0; i<13;i++){
+            while(ans[i]>0){
+                a += alph[i];
+                ans[i]--;
+            }
+        }
+        return a;
+    }
+};

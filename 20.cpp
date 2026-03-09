ee49c9383442cc01253100de71cff0d2a952d879
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-09 20:55:17 +0800

    Update 20.cpp

--- 20.cpp
+++ 20.cpp
@@ -23,3 +23,38 @@ public:
         return true;
     }
 };
+
+
+
+最優解:
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> temp;
+        
+        for (char a : s) {
+            // 1. 遇到左括號：我們把「預期的右括號」推進罐子裡
+            if (a == '(') {
+                temp.push(')');
+            } 
+            else if (a == '[') {
+                temp.push(']');
+            } 
+            else if (a == '{') {
+                temp.push('}');
+            } 
+            // 2. 遇到右括號：準備驗證！
+            else {
+                // 【防護罩】如果罐子已經空了（代表右括號太多），或者跟預期的不符
+                if (temp.empty() || temp.top() != a) {
+                    return false; // 直接判死刑
+                }
+                // 驗證通過，把最上面的預期括號丟掉
+                temp.pop();
+            }
+        }
+        
+        // 3. 全部跑完後，罐子必須是空的（代表所有左括號都有找到伴）
+        return temp.empty(); 
+    }
+};

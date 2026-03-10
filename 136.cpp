2b8e71176c2f594e5814259b8e061b1f392a636a
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-11 01:58:02 +0800

    Update 136.cpp

--- 136.cpp
+++ 136.cpp
@@ -15,3 +15,17 @@ public:
         return 0;
     }
 };
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i=0 ;i<n;i++){
+            sum ^= nums[i];
+        }
+        return sum;
+    }
+};
+用XOR，A ^ A 會變成0，同時有交換率，所以可以倆倆抵銷，所以對奇數次無效。

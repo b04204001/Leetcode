89f9222b72206ba64c705e5a7565dcd6139da171
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-11 01:28:49 +0800

    Create 104.cpp

---
+++ 104.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int getHight(TreeNode* root ){
+        if (root == NULL) {
+            return 0; // 若定義空樹高度為-1，則此處返回 -1
+        }
+        int left = getHight(root->left);
+        int right = getHight(root->right);
+        if ( left > right){
+            return left + 1;
+        }
+        else{
+            return right + 1;
+        }
+    }
+
+    int maxDepth(TreeNode* root) {
+        int h = getHight(root);
+        return h;
+    }
+};

974b837630873b86f53c3ec9c7a7d81c03686f7d
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-11 08:11:47 +0800

    Update 283.cpp

--- 283.cpp
+++ 283.cpp
@@ -4,6 +4,7 @@ public:
         int n= nums.size();
         int j = 0; 
         for(int i=0;i<n;i++){
+            //非0交換，代表0時j會停在0處，之後不斷把非0往前放
             if(nums[i] != 0){
                 swap(nums[i],nums[j++]);
             }

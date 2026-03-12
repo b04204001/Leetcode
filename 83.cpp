64a0ccc7637c42266a6695e1e7e30cceab267d20
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-12 19:54:44 +0800

    Create 83.cpp

---
+++ 83.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) return head;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode * prev = &dummy;
+        ListNode* curr = head;
+        while(curr != nullptr && curr->next != nullptr){
+            if (curr->val == curr->next->val ){
+                prev->next = curr->next;
+                curr = prev->next;
+            }
+            else{
+                prev = prev->next;
+                curr = prev->next;
+            }
+
+        }
+        return dummy.next;
+    }
+};

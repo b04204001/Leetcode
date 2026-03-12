c811836c2a88d1f56e53b1fa1927dacdae45dead
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-12 21:41:52 +0800

    Update 82.cpp

--- 82.cpp
+++ 82.cpp
@@ -36,3 +36,43 @@ public:
         return dummy.next;
     }
 };
+
+
+
+ㄥ
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+        while(curr != nullptr){
+            //把curr移到重複的最後一格
+            while(curr->next != nullptr && curr->val == curr->next->val){
+                curr = curr->next;
+            }
+            //無重複，直接prev往前移動，對其等下的curr往前
+            if(prev->next == curr){
+                prev = prev->next;
+            }
+            //有重複，把prev跟curr->next連接，這樣重複都被踢掉。
+            else{
+                prev->next = curr->next;
+            }
+            //curr往前下一格，有重複因為中間刪掉，所以她還在前一格
+            curr = curr->next;
+        }
+        return dummy.next;
+    }
+};

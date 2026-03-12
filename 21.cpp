981e856b9f3893c09f0ea0b85bca21091d9a10ba
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-12 22:27:57 +0800

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -31,3 +31,64 @@ public:
 		}
 	}
 };	
+
+
+
+//2刷自己寫的
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+        ListNode dummy(0);
+        ListNode* p = list1; //下一個被接
+        ListNode* q = list2; //下一個被接
+        ListNode* r = &dummy; //排好最後一格
+
+        //處理開頭
+        if(list1->val <= list2->val){
+            dummy.next = list1;
+            p = p->next;
+        }
+        else{
+            dummy.next = list2;
+            q = q->next;
+        }
+        r = r->next;
+        //正式開始
+        while(p != nullptr || q != nullptr){
+            if(p != nullptr && q != nullptr ){
+                if( p->val <= q->val ){
+                    r->next = p;
+                    p = p->next;
+                }
+                else if( q->val < p->val ){
+                    r->next = q;
+                    q=q->next;
+                }
+            }
+            else{
+                if(q == nullptr  ){
+                    r->next = p;
+                    p = p->next;
+                }
+                else{
+                    r->next = q;
+                    q = q->next;
+                }
+            }
+            r = r->next;
+        }
+        return dummy.next;
+    }
+};

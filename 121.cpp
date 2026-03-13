f5cfe3b275f02f470262229de43712eb888f25fc
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-13 23:04:54 +0800

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -14,3 +14,22 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price= prices[0];
+        int profit = 0;
+        for(int i=1;i<prices.size();i++){
+            if (prices[i] < min_price){
+                min_price = prices[i];
+            }
+            // < 更換買的價格
+            else{
+                profit = max( (prices[i] - min_price) , profit);
+            }
+        }
+        return profit;
+    }
+};

01d13f474ae0249b3a1425217923503ba418ce0c
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-13 23:32:36 +0800

    Create 122.cpp

---
+++ 122.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int min_price = prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] <= min_price){
+                min_price = prices[i];
+            }
+            else{
+                profit += prices[i] - min_price;
+                min_price = prices[i];
+            }
+        }
+        return profit;
+
+    }
+};

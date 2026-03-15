881b1774d2958948d21dae6cc9f9bf45fdab8d06
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-15 22:38:18 +0800

    Update 142.cpp

--- 142.cpp
+++ 142.cpp
@@ -26,3 +26,34 @@ public:
     return nullptr;
     }
 };
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*slow = head;
+        ListNode*fast = head;
+        //確認cycle
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) break;
+        }
+        if((fast == nullptr || fast->next == nullptr) ) return nullptr;
+        while(head != slow){
+            head = head->next;
+            slow = slow->next;
+        }
+        return head;
+    }
+};

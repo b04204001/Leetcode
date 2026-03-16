2ce34c0eade63e00a8e91f3386e8833fa2cd736d
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-16 19:23:36 +0800

    Update 191.cpp

--- 191.cpp
+++ 191.cpp
@@ -9,3 +9,17 @@ public:
         return ans;
     }
 };
+//上面mask做法，記錄用&1
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count= 0;
+        while( n!= 0){
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+};
+//直接用 n & n-1 =0 把1削掉看做幾次

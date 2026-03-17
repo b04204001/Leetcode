c9a868fb589b0a0246c2d8db331867cf9cf8f422
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-18 00:34:37 +0800

    Update 342.cpp

--- 342.cpp
+++ 342.cpp
@@ -8,3 +8,23 @@ public:
         return n == 1;
     }
 };
+
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n<=0) return false;
+        if(n==1) return true;
+        //只有一個1，在3.5.7 ....
+        if(( n & (n-1)) !=0 ) return false;
+        else{
+            n>>=1;
+            while(n>0){
+                if( (n & 1 ) != 0 ) return false;
+                n>>=2;
+            }
+        }
+        return true;
+    }
+};

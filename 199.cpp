935abf7fcb998c54994aec0fed9d115cefc085af
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-20 10:41:58 +0800

    Create 199.cpp

---
+++ 199.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if(root == nullptr) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int levelsize = q.size();
+            vector<int> level;
+            for(int i =0;i<levelsize; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                level.push_back(curr->val);
+
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            reverse(level.begin(),level.end());
+            ans.push_back(level[0]);
+        }
+        return ans;
+    }
+};

ac33ff6e1851172c6b5e6f09e4e43f035a0df4ee
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-22 22:26:18 +0800

    Create 49.cpp

---
+++ 49.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        //用hash紀錄
+        unordered_map<string,vector<string>> mp;
+        for(string s : strs){
+            string temp = s;
+            //相同的sort結果會相同
+            sort(temp.begin(),temp.end());
+            //加到second
+            mp[temp].push_back(s);
+        }
+        //遍歷hash，把second加到ans
+        for(auto x : mp){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};

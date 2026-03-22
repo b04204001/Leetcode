bb04fc9556d8b8765e4f0878155c9a7e31873bb9
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-22 17:15:30 +0800

    Create 62.cpp

---
+++ 62.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int path[m][n];
+        path[0][0] = 1;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i==0 && j ==0) continue;
+                if(i == 0){
+                    path[i][j] = path[i][j-1] ;
+                }
+                else if (j == 0){
+                    path[i][j] = path[i-1][j] ;
+                }
+                else{
+                    path[i][j] = path[i-1][j] + path[i][j-1];
+                }
+            }
+        }
+        return path[m-1][n-1];
+    }
+};
+
+
+//優解
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};

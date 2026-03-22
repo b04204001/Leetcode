f912d5870a80d1243c3844089c5ea6bed4fdd379
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-22 22:09:16 +0800

    Create 56.cpp

---
+++ 56.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        if ( intervals.empty() ) return ans;
+        // Step 1: 先依照起點排序
+        sort(intervals.begin(), intervals.end());
+
+        // Step 2: 先把第一個區間當作目前合併中的區間
+        vector<int> last = intervals[0];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            vector<int>& cur = intervals[i];
+
+            // 如果重疊：更新目前區間的尾巴
+            if (last[1] >= cur[0]) {
+                last[1] = max(last[1], cur[1]);
+            }
+            // 如果不重疊：代表 last 已經結束，放進答案
+            else {
+                ans.push_back(last);
+                last = cur;
+            }
+        }
+
+        // Step 3: 最後一段別忘了放進答案
+        ans.push_back(last);
+
+
+        return ans;;
+    }
+};

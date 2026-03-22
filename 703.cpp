0913db3f7f23806b5433060401b1dd90000ad4df
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-22 20:39:11 +0800

    Update 703.cpp

--- 703.cpp
+++ 703.cpp
@@ -22,3 +22,29 @@ public:
  * int param_1 = obj->add(val);
  */
 
+
+
+class KthLargest {
+public:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int i : nums) {
+            pq.push(i);
+            if(pq.size()>k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>k) pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */

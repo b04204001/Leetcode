976d9d135cb75075bd905bccd60d8f8b194a2143
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-23 22:00:20 +0800

    Update 198.cpp

--- 198.cpp
+++ 198.cpp
@@ -24,3 +24,20 @@ public:
         return max[n-1];
     }
 };
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,0);
+        //指闖一晚
+        dp[0] = nums[0];
+        if( n == 1) return dp[0];
+        dp[1] = max( dp[0], nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max( dp[i-1]  , dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
+    }
+};

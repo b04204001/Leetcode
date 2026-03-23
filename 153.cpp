46993e94a39ded26dfd9e4809431357e8bd026ab
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-24 00:11:30 +0800

    Update 153.cpp

--- 153.cpp
+++ 153.cpp
@@ -3,20 +3,40 @@ public:
     int BinSearch(vector<int>& nums , int l, int r){
         if (l == r) return nums[l];
         int mid = l + (r - l) /2;
-        int lmin;
-        int rmin;
         //中間比較大，代表最小在右半
         if(nums[mid] > nums[r]){
-            lmin = BinSearch(nums,mid+1,r);
+            return BinSearch(nums, mid + 1, r);
         }
         //中間比較小，代表最小在左半
         else{
-            rmin = BinSearch(nums,l,mid);
+           return BinSearch(nums, l, mid);
         }
-        return min(lmin,rmin);
     }
     int findMin(vector<int>& nums) {
         int n = nums.size(); 
         return BinSearch(nums,0,n-1);
     }
 };
+
+//優解
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r) {
+            if (nums[l] < nums[r]) return nums[l];
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return nums[l];
+    }
+};
+

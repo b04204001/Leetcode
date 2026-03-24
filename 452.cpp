59bd5d4a092d52b961b3e094169b01429df4ef79
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-24 18:11:42 +0800

    Update 452.cpp

--- 452.cpp
+++ 452.cpp
@@ -29,3 +29,28 @@ public:
         return cnt;
     }
 };
+
+
+//簡潔版本
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.empty()) return 0;
+
+        sort(points.begin(), points.end());
+
+        int cnt = 1;
+        int end = points[0][1];
+
+        for (int i = 1; i < points.size(); i++) {
+            if (points[i][0] <= end) {
+                end = min(end, points[i][1]);
+            } else {
+                cnt++;
+                end = points[i][1];
+            }
+        }
+
+        return cnt;
+    }
+};

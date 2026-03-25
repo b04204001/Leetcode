fcd17097bb09515c8ea7ae656f6b6a4b6dde8913
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-25 20:43:37 +0800

    Update 152.cpp

--- 152.cpp
+++ 152.cpp
@@ -24,3 +24,27 @@ public:
         return ans;
     }
 };
+
+//優等解
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int curMax = nums[0];
+        int curMin = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            int x = nums[i];
+
+            int tmpMax = max({x, curMax * x, curMin * x});
+            int tmpMin = min({x, curMax * x, curMin * x});
+
+            curMax = tmpMax;
+            curMin = tmpMin;
+
+            ans = max(ans, curMax);
+        }
+
+        return ans;
+    }
+};

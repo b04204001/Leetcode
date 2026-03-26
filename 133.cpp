11577b1633ffae3dcc787834a5d216f96df7fc00
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-26 22:01:34 +0800

    Update 133.cpp

--- 133.cpp
+++ 133.cpp
@@ -5,15 +5,15 @@ public:
     unordered_map<Node*, Node*> mp;
 
     Node* dfs(Node* node) {
+        //邊界條件
         if (node == nullptr) return nullptr;
-
-        // 已經複製過，直接回傳
+        // 節點已經複製過，直接回傳
         if (mp.count(node)) return mp[node];
-
-        // 建立新節點
+        
+        //這層: 複製節點
+        // 建立新節點，並加入hash代表複製成功。
         Node* clone = new Node(node->val);
         mp[node] = clone;
-
         // 複製鄰居
         for (auto nei : node->neighbors) {
             clone->neighbors.push_back(dfs(nei));

50cd1d21b1c7119f2396f88a3abe825f06a69e4f
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-27 08:41:50 +0800

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,31 @@
+
+
+
+//中心擴展法，另外紀錄最長並記錄開始結束，最後用substr回傳
+class Solution {
+public:
+    int start = 0;
+    int maxLen = 0;
+    void expand(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            int len = right - left + 1;
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = left;
+            }
+
+            left--;
+            right++;
+        }
+    }
+
+    string longestPalindrome(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            expand(s, i, i);     // 奇數
+            expand(s, i, i + 1); // 偶數
+        }
+
+        return s.substr(start, maxLen);
+    }
+};

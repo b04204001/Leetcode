ee936cb6dbfcad1bddd2dc94a8efd41d75f8407d
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-29 10:24:46 +0800

    Update 2839.cpp

--- 2839.cpp
+++ 2839.cpp
@@ -2,14 +2,30 @@ class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
         // 2種交換 (0,2) (1,3) 
+        //不換
         if( s1 == s2) return true;
+        //只換02
         swap(s1[0],s1[2]);
         if( s1 == s2) return true;
+        //換了02 + 13
         swap(s1[1],s1[3]);
         if( s1 == s2) return true;
+        //只換13
         swap(s1[0],s1[2]);
         if( s1 == s2) return true;
         return false;
     }
 };
 
+
+
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        return ((s1[0] == s2[0] && s1[2] == s2[2]) ||
+                (s1[0] == s2[2] && s1[2] == s2[0]))
+            &&
+               ((s1[1] == s2[1] && s1[3] == s2[3]) ||
+                (s1[1] == s2[3] && s1[3] == s2[1]));
+    }
+};

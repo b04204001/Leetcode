5fc736b79b7e7a096abb3352acc029b68bda2005
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-30 23:58:03 +0800

    Update 222.cpp

--- 222.cpp
+++ 222.cpp
@@ -1,30 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//暴力解O(n)
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if (root == nullptr) return 0;
-        int countl = 0;
-        int countr = 0;
-        if (root->left != nullptr ) {
-            countl = countNodes(root->left);
-        }
+        if(root == nullptr) return 0;
         
-        if (root->right != nullptr ) {
-            countr = countNodes(root->right);
-        }
-        
-        int count = countl + countr + 1;
-        return count;
+        return 1+ countNodes(root->left) + countNodes(root->right);
     }
 };
 

5dc0d2948f8158343747991d3f71e3a062d2f42f
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-03-31 01:29:37 +0800

    Update 342.cpp

--- 342.cpp
+++ 342.cpp
@@ -9,6 +9,15 @@ public:
     }
 };
 
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        if ((n & (n - 1)) != 0) return false;
+        return (n - 1) % 3 == 0;
+    }
+};
+
 
 
 class Solution {

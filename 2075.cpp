d6733a4c8c950e696b1d373b6e8e8692b109a8e6
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-04-05 13:59:22 +0800

    Update 2075.cpp

--- 2075.cpp
+++ 2075.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
+        //先計算好rows * cols
         int n = encodedText.length();
         int cols = n / rows;
         string ans = "";
 
-        // 1. 遍歷第一列的每個字元作為「斜線起點」
+        // 1. 遍歷第一列的每個字元作為「斜線起點」(0,0) ~ (0,cols)
         for (int j = 0; j < cols; ++j) {
             // 2. 從起點 (0, j) 開始往右下方移動
             // 每次 row+1, col+1，兩者增加的量相同，設為 i
+            // 0,0 1,1 2,2 ...   0,1 1,2 2,3 ...
             for (int r = 0, c = j; r < rows && c < cols; ++r, ++c) {
                 // 計算在一維字串中的位置
                 ans += encodedText[r * cols + c];

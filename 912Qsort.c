1204bce6ce179b2b0ddc262dd5b68058ac71ddab
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-04-07 22:08:46 +0800

    Create 912Qsort.c

---
+++ 912Qsort.c
@@ -0,0 +1,52 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void swap(int * a , int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+void Qsort(int* nums ,int left , int right){
+    if (left >= right) return;
+    //選定中間
+    
+    //左中
+    if( *(nums + left) > *(nums + (right + left)/2 )) {  
+        swap((nums + left) , (nums + (right + left)/2 ) );
+    }
+    //左右
+    if( *(nums + left) < *(nums + right) ){
+        swap((nums + left) , (nums + right ));
+    } 
+    //中又
+    if( *(nums + (right + left)/2 ) > *(nums + right)) {  
+        swap((nums + (right + left)/2 ) , (nums + right) );
+    }
+    int pivot= *(nums + left);
+    //開始n次
+    int l = left + 1;
+    int r = right;
+    while( l <= r){
+        while( l <= r && *(nums + r) > pivot){
+            r--;
+        }
+        while(l <= r && *(nums + l) < pivot){
+            l++;
+        }
+        if(l <= r){
+            swap((nums + l) , (nums +r));
+            l++;
+            r--;
+        }
+    }
+    swap(nums + left , nums + r);
+    Qsort(nums , left , r-1 );
+    Qsort(nums , r+1 , right);
+    return;
+}
+int* sortArray(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+    Qsort(nums , 0 , numsSize -1 );
+    return nums;
+}

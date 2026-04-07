a424585e193065e1554e15cb2879656e638b027f
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-04-07 12:11:17 +0800

    Create 151.cpp

---
+++ 151.cpp
@@ -0,0 +1,49 @@
+void reverse(char* s , char*e){
+    while(s < e){
+        char t = *s;
+        *s = *e;
+        *e = t;
+        s++;
+        e--;
+    }
+}
+
+char* reverseWords(char* s) {
+    char *read = s;
+    char * write = s;
+    //清理空格
+    // 1. 去掉前導空白
+    while (*read == ' ') read++;
+    //2.中間
+    while(*read){
+        if(*read != ' '){
+            *write++ = *read++;
+        }
+        else{
+            *write++ = *read;
+            while (*read == ' ') read++;
+        }
+    }
+    //3.尾巴
+    while (write > s && *(write-1) == ' ') write--;
+    *write = '\0';
+    //大反轉
+    reverse(s,write - 1);
+
+    //各自word反轉
+    char* start = s;
+    char* end = s;
+    while(*end != '\0'){
+        if(*end == ' '){
+            reverse(start,end-1);
+            end++;
+            start = end;
+        }
+        else{
+            end++;
+        }
+    }
+    reverse(start,end-1);
+    return s;
+}
+

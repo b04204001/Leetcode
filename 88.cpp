eb04e631197d1c1de981790af8de3619480eab19
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-04-07 22:22:16 +0800

    Update 88.cpp

--- 88.cpp
+++ 88.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int j=0;j<n;j++){
-            nums1.pop_back();
+        int i = m - 1; // nums1 最後一個有效元素的 index
+        int j = n - 1; // nums2 最後一個元素的 index
+        int k = m + n - 1; // nums1 整個陣列最後一個位置
+        
+        // 從後面開始比，誰大就放最後面
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        for(int i=0;i<n;i++){
-            nums1.push_back(nums2[i]);
+        
+        // 如果 nums2 還有剩，把它們補進去
+        // (如果是 nums1 有剩，不用動，因為它們本來就在 nums1 裡面且已經排好了)
+        while(j >= 0) {
+            nums1[k--] = nums2[j--];
         }
-        sort(nums1.begin(),nums1.end());
-
     }
 };

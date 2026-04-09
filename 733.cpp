fa4e2c1531b02890f671e03e6e0076facdc4ccc8
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-04-09 22:01:44 +0800

    Update 733.cpp

--- 733.cpp
+++ 733.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& image, int i, int j, int target ,int color) {
+        int m = image.size();
+        int n = image[0].size();
+        if(i >= m || j>= n || i<0 || j <0) return;
+        if(image[i][j] == color) return;
+        if(image[i][j] != target ) return;
+        image[i][j]=color;
+        dfs(image,i-1,j,target,color);
+        dfs(image,i+1,j,target,color);
+        dfs(image,i,j-1,target,color);
+        dfs(image,i,j+1,target,color);
+        return;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        dfs(image,sr,sc,image[sr][sc],color);
+        return image;
+    }
+};
+
+//解答
+class Solution {
+public:
     void dfs(vector<vector<int>>& image , int i ,int j, int same, int color){
         int m = image.size();
         int n = image[0].size();

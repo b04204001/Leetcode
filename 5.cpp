95e1a035ee2d79134503b65c163ef2aa1cf8a339
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-04-15 23:09:11 +0800

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,4 +1,35 @@
+class Solution {
+public:
 
+    int expand(string s,int i , int j){
+        while( i >= 0 && j < s.length() && s[i] == s[j]){
+            i--;
+            j++;
+        }
+        return (j-1) - (i+1) + 1;
+    }
+    string longestPalindrome(string s) {
+        int start = 0;
+        int max_len = 0;
+        for(int i =0;i<s.length();i++){
+            int a = expand(s,i,i);
+            if( a > max_len){
+                start = i - a/2;
+                max_len = a;
+            }
+            int b = expand(s,i,i+1);
+            if(b > max_len){
+                start = i - (b - 1) / 2;
+                max_len = b;
+            }
+        }
+        string ans="";
+        for(int i = start ; i < start + max_len;i++ ){
+            ans+= s[i];
+        }
+        return ans;
+    }
+};
 
 
 //中心擴展法，另外紀錄最長並記錄開始結束，最後用substr回傳

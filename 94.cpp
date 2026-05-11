a72bf20b042920e1cfc902b34514f3ca32f83cf2
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-12 01:45:49 +0800

    Update 94.cpp

--- 94.cpp
+++ 94.cpp
@@ -25,3 +25,54 @@ public:
         return ans;
     }
 };
+
+
+// stack法
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* t,vector<int>& ans){
+        if(t == NULL) return;
+        dfs(t->left,ans);
+        ans.push_back(t->val);
+        dfs(t->right,ans);
+        return;
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        //遞回法
+        //dfs(root, ans);
+
+        //stack法
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+
+        while(!st.empty() || curr != nullptr){
+            // 1. 一路向左：把左側所有節點都壓入棧
+            while(curr != nullptr){
+                st.push(curr);
+                curr = curr->left;
+            }
+            // 2. 觸底反彈：取出最後一個左節點（或是目前的根節點）
+            curr = st.top();
+            st.pop();
+
+            // 3. 處理數值
+            ans.push_back(curr->val);
+
+            // 4. 轉向右子樹：重複上述邏輯
+            curr = curr->right;
+        }
+        return ans;
+    }
+};

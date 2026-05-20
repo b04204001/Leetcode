3a0c282bcd902b80a974132aab7049f08ebd627a
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-21 01:04:08 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -18,6 +18,22 @@ public:
 
 
 //hash版本
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(int i = 0; i < nums.size();i++){
+            int diff = target - nums[i];
+            if(mp.count(diff)){
+                ans.push_back(i);
+                ans.push_back(mp[diff]);
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
 
 class Solution {
 public:

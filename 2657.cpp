8255e8df9f7eae3eabbaa2871eee6e00e6cb999d
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-20 09:13:19 +0800

    Update 2657.cpp

--- 2657.cpp
+++ 2657.cpp
@@ -20,3 +20,33 @@ public:
         return common;
     }
 };
+
+
+//最優解
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> common(n);
+        vector<int> freq(n + 1, 0); // 題目給定數字範圍為 1 到 n
+        int current_common = 0;
+
+        for (int i = 0; i < n; i++) {
+            // 處理陣列 A 的元素
+            freq[A[i]]++;
+            if (freq[A[i]] == 2) {
+                current_common++;
+            }
+
+            // 處理陣列 B 的元素
+            freq[B[i]]++;
+            if (freq[B[i]] == 2) {
+                current_common++;
+            }
+
+            common[i] = current_common;
+        }
+
+        return common;
+    }
+};

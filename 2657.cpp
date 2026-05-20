638b24a820970eb02099e69d65cbc9d135b58260
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-20 09:17:39 +0800

    Update 2657.cpp

--- 2657.cpp
+++ 2657.cpp
@@ -27,24 +27,31 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        vector<int> common(n);
-        vector<int> freq(n + 1, 0); // 題目給定數字範圍為 1 到 n
-        int current_common = 0;
+        vector<int> common;
+        
+        // 1. 宣告一個全域的計數陣列，大小為 n + 1，初始值都是 0
+        // 它會隨著迴圈前進持續累積，不需要每輪重新計算
+        vector<int> count(n + 1, 0); 
+        
+        int cnt = 0; // 記錄當前累積的共同數字數量
 
+        // 2. 只用「一層」迴圈遍歷
         for (int i = 0; i < n; i++) {
-            // 處理陣列 A 的元素
-            freq[A[i]]++;
-            if (freq[A[i]] == 2) {
-                current_common++;
+            
+            // 處理 A[i]：加 1
+            count[A[i]]++;
+            if (count[A[i]] == 0) { // 如果變回 0，代表跟之前的 B 配對成功
+                cnt++;
             }
 
-            // 處理陣列 B 的元素
-            freq[B[i]]++;
-            if (freq[B[i]] == 2) {
-                current_common++;
+            // 處理 B[i]：減 1
+            count[B[i]]--;
+            if (count[B[i]] == 0) { // 如果變回 0，代表跟之前的 A 配對成功
+                cnt++;
             }
 
-            common[i] = current_common;
+            // 將當前的結果放入答案中
+            common.push_back(cnt);
         }
 
         return common;

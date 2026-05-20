cb366e12ce3cfcde1aeaee70c25787e97864fa84
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-21 01:12:03 +0800

    Update 217.cpp

--- 217.cpp
+++ 217.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp.insert( nums[i]);
+        unordered_map<int,int>mp;
+        for(int i =0;i<nums.size();i++){
+            if(mp.count(nums[i])) return true;
+            mp[nums[i]]++;
         }
-        if(mp.size() < nums.size()){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
+        return false;
     }
 };

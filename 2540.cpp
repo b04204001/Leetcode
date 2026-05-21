4b1d7ced1b93b8afb503d39e0eecaa7435dfc584
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-21 10:32:06 +0800

    Update 2540.cpp

--- 2540.cpp
+++ 2540.cpp
@@ -14,3 +14,29 @@ public:
         return max_val == INT_MAX? -1 : max_val;
     }
 };
+
+//最優解 雙指標
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+        int max_val = INT_MAX;
+        while(i<n && j <m){
+            if(nums1[i] == nums2[j]){
+                max_val = min(max_val , nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] > nums2[j]){
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return max_val == INT_MAX? -1 : max_val;
+    }
+};

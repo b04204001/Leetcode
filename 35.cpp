d6759713efb065f5f729547c6f11e2e075e33528
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-23 02:01:38 +0800

    Update 35.cpp

--- 35.cpp
+++ 35.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int ans;
-        for(int i = 0;i<nums.size();i++){
-            if(target <= nums[i]){
-                ans =i;
-                break;
+        int i = 0;
+        int j = nums.size()-1;
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            if( nums[mid] == target){
+                return mid;
             }
-            
+            else if ( nums[mid] > target){
+                j = mid - 1;
+            }
+            else if ( nums[mid] < target ){
+                i = mid+ 1;
+            }
         }
-        return ans;
+        return i;
     }
 };

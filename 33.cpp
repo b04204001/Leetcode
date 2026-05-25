264c19f6d896914b5d3f3fc5800a311813555135
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-25 11:09:55 +0800

    Update 33.cpp

--- 33.cpp
+++ 33.cpp
@@ -1,6 +1,55 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size()-1;
+        while(i<=j){
+            int mid = i +(j-i)/2;
+            if(target == nums[mid]) return mid;
+            //左半邊轉點，右半遞增
+            if(nums[i] > nums[mid]){
+                //，比mid小，一定左半
+                if(nums[mid] > target){
+                    j = mid -1;
+                }
+                //比mid大，右半 + 左開頭 
+                else{
+                    //右半
+                    if(target < nums[j]){
+                        i = mid + 1;
+                    }
+                    //左開頭
+                    else{
+                        j = mid - 1;
+                    }
+                }
+            }
+            //右半邊轉點，左半遞增
+            else{
+                // 比mid大，在右半
+                if(nums[mid] < target){
+                    i = mid +1 ;
+                }
+                //左半 或 右半尾
+                else{
+                    // 比i大，右半
+                    if(nums[i] > target){
+                        i = mid + 1;
+                    }
+                    else{
+                        j = mid - 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
+//優解
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size()-1;
         while(left<=right){

805bc5750edad593e1b09770a9735a7f9b8b401f
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-27 14:41:41 +0800

    Update 128.cpp

--- 128.cpp
+++ 128.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> num_set(nums.begin(), nums.end());
+        int length = 0;
+        for(int n : num_set){
+            //是起點
+            if(!num_set.count(n - 1)){
+                int curr_n = n;
+                int curr_len = 1;
+                //去找長度
+                while(num_set.count(curr_n +1)){
+                    curr_n++;
+                    curr_len++;
+                }
+                
+                length = max(length ,curr_len );
+            }
+        }
+        return length;
+    }
+};

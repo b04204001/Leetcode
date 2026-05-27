3cdd54fb0fe4e7dd9eb96652aed1a8e37a84cdb0
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-27 12:01:49 +0800

    Update 155.cpp

--- 155.cpp
+++ 155.cpp
@@ -46,3 +46,39 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+更好:
+class MinStack {
+public:
+    vector<int> st;
+    vector<int> min_st;
+
+    MinStack() { }
+    
+    void push(int val) {
+        st.push_back(val);
+        // 當 min_st 為空，或者新值小於等於當前最小值時，才寫入 min_st
+        if (min_st.empty() || val <= min_st.back()) {
+            min_st.push_back(val);
+        }
+    }
+    
+    void pop() {
+        if (st.empty()) return;
+        
+        // 如果要彈出的值正好是當前最小值，min_st 也要跟著彈出
+        if (st.back() == min_st.back()) {
+            min_st.pop_back();
+        }
+        st.pop_back();
+    }
+    
+    int top() {
+        return st.back(); // 根據 LeetCode 題目要求，呼叫 top 時堆疊必不為空
+    }
+    
+    int getMin() {
+        return min_st.back();
+    }
+};
+

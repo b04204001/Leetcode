02487bce23ffb0c8fad5ca12094d85b5a4a39bb0
b04204001 <60873981+b04204001@users.noreply.github.com>
2026-05-27 10:53:57 +0800

    Update 215.cpp

--- 215.cpp
+++ 215.cpp
@@ -45,3 +45,18 @@ public:
         return nums[0];
     }
 };
+
+//用STL
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int a : nums){
+            pq.push(a);
+        }
+        for(int i = 1 ; i<k ; i ++){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
